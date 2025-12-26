e86845d3d9e9920374dce6a9ea49a1cd2ae966b3
thenootz <danut.iurascu@gmail.com>
2025-12-26 19:48:29 +0100

    IERC20 added

---
+++ contracts/token/FixedPriceToken.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address a) external view returns (uint256);
+    function transfer(address to, uint256 value) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spemder, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+
+    event Transfer(address indexed from, address indexed spender, uint256 value);
+    event Approval(address indexed owner, address indexed, uint256 value);
+
+ }
+
+ contract FixedPriceToken {
+
+
+ }
